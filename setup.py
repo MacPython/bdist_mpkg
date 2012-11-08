1f389775c02b29a13364aa6b6fbffeb242ba9928
Matthew Brett <matthew.brett@gmail.com>
2012-11-07 21:55:57 -0800

    RF: isolate info namespace with dictionary

--- setup.py
+++ setup.py
@@ -6,14 +6,15 @@ import os
 
 # Get version and release info, which is all stored in bdist_mpkg/info.py
 ver_file = os.path.join('bdist_mpkg', 'info.py')
-exec(open(ver_file).read())
+VARS = {}
+exec(open(ver_file).read(), VARS)
 
 setup(
     name="bdist_mpkg",
-    version=__version__,
-    description=DESCRIPTION,
-    long_description=LONG_DESCRIPTION,
-    classifiers=CLASSIFIERS,
+    version=VARS['__version__'],
+    description=VARS['DESCRIPTION'],
+    long_description=VARS['LONG_DESCRIPTION'],
+    classifiers=VARS['CLASSIFIERS'],
     author="Bob Ippolito",
     author_email="pythonmac-sig@python.org",
     url="http://undefined.org/python/#bdist_mpkg",
